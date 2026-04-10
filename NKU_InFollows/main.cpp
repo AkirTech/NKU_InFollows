aaa2c63be70fdf3090970bcd4a90ffc8e22706ac
Akir_ <96360624+AkirTech@users.noreply.github.com>
2026-04-10 06:12:11 +0000

    feature:部分重构main.cpp，即将支持OOBE阶段中途退出，自动保存进度。
    － 建议好好上物理课。

--- NKU_InFollows/main.cpp
+++ NKU_InFollows/main.cpp
@@ -5,6 +5,12 @@
 #include <qdebug.h>
 #include "fileio.h"
 #include "cfgLoader.h"
+
+static const QString pageURLs[50] {
+    QStringLiteral("qrc/");
+    
+}
+
 int main(int argc, char *argv[])
 {
 #if defined(Q_OS_WIN) && QT_VERSION_CHECK(5, 6, 0) <= QT_VERSION && QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
@@ -15,13 +21,21 @@ int main(int argc, char *argv[])
     maincfg.setCfg("appDirPath", QCoreApplication::applicationDirPath());
 	FileIO fileIO;
 
-
+    QString status = maincfg.load("OOBE");
     
 	app.setWindowIcon(QIcon(QCoreApplication::applicationDirPath() + "/NKU_InFollows_icon.png"));
 	qInfo() << "Application directory path: " << QCoreApplication::applicationDirPath();
 
     QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/qt/qml/nku_infollows/main.qml")));
+    if (status == QStringLiteral("fresh")){
+        engine.load(QUrl(QStringLiteral("......")))
+    }
+    else if (status == QStringLiteral("evaluate")){
+        int progress = maincfg.load("oobe_progress").toInt();
+        
+        engine.load(QUrl(pageURLs[progress]));
+    }
+    // engine.load(QUrl(QStringLiteral("qrc:/qt/qml/nku_infollows/main.qml")));
 	engine.rootContext()->setContextProperty("appDirPath", QCoreApplication::applicationDirPath());
 	engine.rootContext()->setContextProperty("FileIO", &fileIO);
 	engine.rootContext()->setContextProperty("maincfg", &maincfg);
